c4f07c8b0ef52d8075a01ee9e8aade0ccacae5ea
DawidGora09 <dawid.gora1999@gmail.com>
2022-01-10 11:37:56 +0100

    apply code review's suggested changes

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,11 +2,11 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    public static final int ARRAY_LENGTH = 12;
-    public static final int EMPTY = -1;
-    private final int[] numbers = new int[ARRAY_LENGTH];
+    public static final int MAX_ARRAY_LENGTH = 12;
+    public static final int EMPTY_RETURN_VALUE = -1;
+    private final int[] numbers = new int[MAX_ARRAY_LENGTH];
 
-    public int total = EMPTY;
+    public int total = EMPTY_RETURN_VALUE;
 
     public void countIn(int in) {
         if (!isFull())
@@ -14,22 +14,22 @@ public class DefaultCountingOutRhymer {
     }
 
     public boolean callCheck() {
-        return total == EMPTY;
+        return total == EMPTY_RETURN_VALUE;
     }
 
     public boolean isFull() {
-        return total == ARRAY_LENGTH - 1;
+        return total == MAX_ARRAY_LENGTH - 1;
     }
 
     protected int peekaboo() {
         if (callCheck())
-            return EMPTY;
+            return EMPTY_RETURN_VALUE;
         return numbers[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return EMPTY;
+            return EMPTY_RETURN_VALUE;
         return numbers[total--];
     }
 
