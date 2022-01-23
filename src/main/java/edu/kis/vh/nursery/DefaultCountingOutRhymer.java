810af32d32ac8ee9e021700ecd3a0f13229f5f8c
Jonasz Potoniec <jonasz.potoniec@gmail.com>
2022-01-23 16:50:11 +0100

    1.4 accesory 1 - changed variables visibility

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,12 +2,12 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    public static final int MAX_ARRAY_LENGTH = 12;
-    public static final int EMPTY = -1;
-    public static final int EMPTY_RETURN_VALUE = -1;
+    private static final int MAX_ARRAY_LENGTH = 12;
+    private static final int EMPTY = -1;
+    private static final int EMPTY_RETURN_VALUE = -1;
     private final int[] numbers = new int[MAX_ARRAY_LENGTH];
 
-    public int total = EMPTY;
+    private int total = EMPTY;
 
     public void countIn(int in) {
         if (!isFull())
