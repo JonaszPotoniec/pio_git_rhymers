294f64db5be0548f4a5e5dd965ab0687831d4213
Jonasz Potoniec <jonasz.potoniec@gmail.com>
2022-01-29 13:01:09 +0100

    1.5.6 - added FIFORhymer documentation

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -1,9 +1,19 @@
 package edu.kis.vh.nursery;
 
+/**
+ * First in first out type rhymer
+ */
 public class FIFORhymer extends DefaultCountingOutRhymer {
 
+
+    /**
+     * Temporary rhymer used to reverse order
+     */
     private final DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
 
+    /**
+     * @return returns first value in contradiction to other rhymers which return last value
+     */
     @Override
     public int countOut() {
         while (!callCheck())
